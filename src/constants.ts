7082fb9e88c1aae555231b3cc9a863ce1bbc878d
Taariq Elliott <codebytaariq@gmail.com>
2026-02-25 00:43:26 -0500

    deleted public

--- src/constants.ts
+++ src/constants.ts
@@ -35,10 +35,10 @@ export const NEPTUNE_DIST = 279900;
 export const NEPTUNE_ROTATION_SPEED = 0.00000112;
 
 export class Planet {
-  public size: [number, number, number];
-  public position: [number, number, number];
-  public texture: string;
-  public rotationSpeed: number;
+  size: [number, number, number];
+  position: [number, number, number];
+  texture: string;
+  rotationSpeed: number;
 
   constructor(
     size: [number, number, number],
@@ -56,62 +56,62 @@ export class Planet {
 export const sunDetails = new Planet(
   [SUN_SIZE, SUN_SIZE, SUN_SIZE],
   [SUN_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_sun.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_sun.jpg",
   SUN_ROTATION_SPEED
 );
 
 export const mercuryDetails = new Planet(
   [MERCURY_SIZE, MERCURY_SIZE, MERCURY_SIZE],
   [MERCURY_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_mercury.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_mercury.jpg",
   MERCURY_ROTATION_SPEED
 );
 
 export const venusDetails = new Planet(
   [VENUS_SIZE, VENUS_SIZE, VENUS_SIZE],
   [VENUS_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_venus_surface.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_venus_surface.jpg",
   VENUS_ROTATION_SPEED
 );
 
 export const earthDetails = new Planet(
   [EARTH_SIZE, EARTH_SIZE, EARTH_SIZE],
   [EARTH_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_earth_daymap.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_earth_daymap.jpg",
   EARTH_ROTATION_SPEED
 );
 
 export const marsDetails = new Planet(
   [MARS_SIZE, MARS_SIZE, MARS_SIZE],
   [MARS_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_mars.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_mars.jpg",
   MARS_ROTATION_SPEED
 );
 
 export const jupiterDetails = new Planet(
   [JUPITER_SIZE, JUPITER_SIZE, JUPITER_SIZE],
   [JUPITER_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_jupiter.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_jupiter.jpg",
   JUPITER_ROTATION_SPEED
 );
 
 export const saturnDetails = new Planet(
   [SATURN_SIZE, SATURN_SIZE, SATURN_SIZE],
   [SATURN_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_saturn.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_saturn.jpg",
   SATURN_ROTATION_SPEED
 );
 
 export const uranusDetails = new Planet(
   [URANUS_SIZE, URANUS_SIZE, URANUS_SIZE],
   [URANUS_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_uranus.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_uranus.jpg",
   URANUS_ROTATION_SPEED
 );
 
 export const neptuneDetails = new Planet(
   [NEPTUNE_SIZE, NEPTUNE_SIZE, NEPTUNE_SIZE],
   [NEPTUNE_DIST, 0, 0],
-  "/public/planet-textures/Solarsystemscope_texture_2k_neptune.jpg",
+  "/planet-textures/Solarsystemscope_texture_2k_neptune.jpg",
   NEPTUNE_ROTATION_SPEED
 );
